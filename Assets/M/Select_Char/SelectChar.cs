51aa24ff3d3d1474fd0ba32efa6df3ac5d2bade1
kid <nakid001@gmail.com>
2017-05-28 09:47:31 +0700

    add selectchar.cs

---
+++ Assets/M/Select_Char/SelectChar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectChar : MonoBehaviour {
+
+    public void LoadSelectGame1()
+    {
+        SceneManager.LoadScene("SelectGame");
+        MPlayerController.selectchar = 1;
+    }
+    public void LoadSelectGame2()
+    {
+        SceneManager.LoadScene("SelectGame");
+        MPlayerController.selectchar = 2;
+    }
+    public void LoadSelectGame3()
+    {
+        SceneManager.LoadScene("SelectGame");
+        MPlayerController.selectchar = 3;
+    }
+    public void LoadSelectGame4()
+    {
+        SceneManager.LoadScene("SelectGame");
+        MPlayerController.selectchar = 4;
+    }
+    public void LoadSelectGame5()
+    {
+        SceneManager.LoadScene("SelectGame");
+        MPlayerController.selectchar = 5;
+    }
+}
